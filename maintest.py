5cad10ae4e81604d3103886cc32faeaeaa3d3995
[sgelhaus] <[stge0958@colorado.edu]>
2015-03-14 16:05:55 -0600

    -SG and -DS

--- maintest.py
+++ maintest.py
@@ -2,34 +2,31 @@
 
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", # your host, usually localhost
-                     user="root", # your username
-                      passwd="", # your password
-                      db="RT") # name of the data base
+db = MySQLdb.connect(host="localhost", 
+                     user="root",
+                      passwd="", 
+                      db="RT")
 cur = db.cursor() 
 cur.execute("SELECT * FROM tracks")
 
-# print all the first cell of all the rows
-#for row in cur.fetchall() :
-#    data = row[0], row[1], row[2], row[3], row[4], row[5]
-    
 #db.close()
 
 print "Content-type: text/html"
 
-contents='''
+contents1='''
 
 <html>
-<head>
+<body>
+	<center>
 	<form id='trackList'>
     <select size=3>
     '''    
-print contents
+print contents1
 
 for row in cur.fetchall() :
 	
 	contents2='''
-    <option>
+    <option onclick=audioHandler()>
     '''
 	print contents2
 
@@ -39,7 +36,50 @@ contents3='''
 			<br>
     </select>
     </form>
-</head>
-</html>
+    </center>
+
 '''
 print contents3
+
+
+javascript_play_1='''
+<script type="text/javascript">
+var audio = new Audio;
+function audioHandler() {
+'''
+db = MySQLdb.connect(host="localhost",
+                     user="root",
+                      passwd="",
+                      db="RT")
+cur = db.cursor() 
+cur.execute("SELECT * FROM tracks where Id=%d")%(1)
+
+javascript_play_2='''
+	audio.setAttribute("src", '<?php echo "./audio/" . $output1[FileName]; ?>');
+	audio.load();
+	audio.play();
+'''
+
+print javascript1
+
+javascript2='''
+}</script>
+'''
+print javascript2
+
+buttons='''
+<center>
+<br>
+<input type="button" onclick="pauseAudio()" value="Pause">
+<input type="button" onclick="playAudio()" value="Play">
+<input type="button" onclick="nextAudio()" value="Next">
+</center>
+'''
+print buttons
+
+
+html_end='''
+</body>
+</html>
+'''
+print html_end
