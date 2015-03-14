2ef6a580a72cbd19833e7e73b87872fc64e9b8bf
[sgelhaus] <[stge0958@colorado.edu]>
2015-03-14 15:01:43 -0600

    -SG and -DS

--- maintest.py
+++ maintest.py
@@ -4,16 +4,16 @@ import MySQLdb
 
 db = MySQLdb.connect(host="localhost", # your host, usually localhost
                      user="root", # your username
-                      passwd="3308", # your password
+                      passwd="", # your password
                       db="RT") # name of the data base
 cur = db.cursor() 
 cur.execute("SELECT * FROM tracks")
 
 # print all the first cell of all the rows
-for row in cur.fetchall() :
-    scrollist = row[0], row[1], row[2], row[3], row[4], row[5]
+#for row in cur.fetchall() :
+#    data = row[0], row[1], row[2], row[3], row[4], row[5]
     
-db.close()
+#db.close()
 
 print "Content-type: text/html"
 
@@ -23,15 +23,23 @@ contents='''
 <head>
 	<form id='trackList'>
     <select size=3>
-    <option> print "Database version : %s " % data <br> </option>
-    type <br>
-    type <br>
-    haha <br>
+    '''    
+print contents
+
+for row in cur.fetchall() :
+	
+	contents2='''
+    <option>
+    '''
+	print contents2
+
+	print row[0], row[1], row[2], row[3], row[4], row[5]
+
+contents3='''
+			<br>
     </select>
     </form>
 </head>
 </html>
-
-
 '''
-print contents
+print contents3
